044bcd424fe24d1fad85c34525227ad15699c067
LAPTOP-VMLVOQLM\Rodrigo <rodrigo.bugueno@sansano.usm.cl>
2023-01-09 02:31:02 -0300

    Se añade la carpeta "entregables" con todo lo necesario para llegar y enviar

--- Tarea_3/Tarea_3.scrs/sources_1/AvgVecOperation.sv
+++
@@ -1,7 +0,0 @@
-module AvgVecOperation(
-    input logic [7:0] A, B,
-    output logic [7:0] tx_data
-    );
-    logic [8:0] sum = A + B;    // one bit more
-    assign tx_data = sum[8:1];
-endmodule
--- Tarea_3/Tarea_3.scrs/sources_1/SumVecOperation.sv
+++
@@ -1,6 +0,0 @@
-module SumVecOperation(
-    input logic [7:0] A, B,
-    output logic [7:0] tx_data
-    );
-    assign tx_data = A + B;
-endmodule
