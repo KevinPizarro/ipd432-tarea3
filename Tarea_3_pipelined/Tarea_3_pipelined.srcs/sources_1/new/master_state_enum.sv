5b5610f4e51a883f8d7ff07372c8374684f0601e
Kevin Pizarro <kevin.pizarroa@sansano.usm.cl>
2023-01-10 16:29:38 -0300

    Reordenamiento, corrección

--- Tarea_3_pipelined/Tarea_3_pipelined.srcs/sources_1/new/master_state_enum.sv
+++
@@ -1,5 +0,0 @@
-package master_state_enum;
-    typedef enum logic [3:0] {Standby, WriteVecA, WriteVecB, ReadVecA, ReadVecB,
-                        SumVec, AvgVec, EucDis, ManDis} enum_state;
-endpackage                  
-
